0adfce6a43c73d4c43bbda332dc3f141951d50f4
Lennii <87238895+zLennii@users.noreply.github.com>
2021-12-04 21:29:43 +0000

    fuck

--- cock.rs
+++ cock.rs
@@ -5,9 +5,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   let image = ImageView::new(ImageInfo::rgb8(1920, 1080), pixel_data);
 
-  // Create a window with default options and display the image.
+  
   let window = create_window("image", Default::default())?;
-  window.set_image("image-001", image)?;
+  window.set_image("promethen.png", image)?;
 
   Ok(())
 }
