a53587ad8402717fd4b4836cfe43120159f99292
Lennii <zlennii.me@gmail.com>
2021-12-04 18:39:23 +0000

    i forgo

--- main.rs
+++ main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello World!");
 }
+
+use show_image::{ImageView, ImageInfo, create_window};
+
+#[show_image::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+  let image = ImageView::new(ImageInfo::rgb8(1920, 1080), pixel_data);
+
+
+  let window = create_window("image", Default::default())?;
+  window.set_image("promethen", image)?;
+
+  Ok(())
+}
